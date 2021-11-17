f575caf073475bdca663a1feb94c8b3874709e5f
team-a4f <oop.batch@gmail.com>
2021-11-17 20:29:10 +0530

    updated sql

--- db_files/tutora_db.sql
+++ db_files/tutora_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 15, 2021 at 02:15 PM
+-- Generation Time: Nov 17, 2021 at 03:57 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -29,11 +29,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `exam_enquiries` (
   `enqID` int(11) NOT NULL,
-  `subject` int(11) NOT NULL,
+  `subject` varchar(25) NOT NULL,
   `exam_date` varchar(100) NOT NULL,
   `enquiry` varchar(255) NOT NULL,
   `studentID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `exam_enquiries`
+--
+
+INSERT INTO `exam_enquiries` (`enqID`, `subject`, `exam_date`, `enquiry`, `studentID`) VALUES
+(1, 'bbb', 'ccc', 'ddd', 12345678),
+(3, 'zzz', 'yyy', 'xxx', 12345678),
+(4, 'xzc', 'xzc', 'zxc', 12345678);
 
 -- --------------------------------------------------------
 
@@ -43,14 +52,21 @@ CREATE TABLE `exam_enquiries` (
 
 CREATE TABLE `registered_students` (
   `studentID` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
   `nic` varchar(15) DEFAULT NULL,
   `phone` int(50) DEFAULT NULL,
   `age` int(3) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
   `gender` varchar(10) DEFAULT NULL,
-  `address` int(255) DEFAULT NULL
+  `address` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `registered_students`
+--
+
+INSERT INTO `registered_students` (`studentID`, `name`, `nic`, `phone`, `age`, `email`, `gender`, `address`) VALUES
+(12345678, 'Anoj', '123456798', 3213213, 23, 'test@test.lk', 'Male', '567, Somewhere, Nowhere.');
 
 --
 -- Indexes for dumped tables
@@ -61,6 +77,7 @@ CREATE TABLE `registered_students` (
 --
 ALTER TABLE `exam_enquiries`
   ADD PRIMARY KEY (`enqID`),
+  ADD UNIQUE KEY `enqID` (`enqID`),
   ADD KEY `studentID` (`studentID`);
 
 --
@@ -74,10 +91,16 @@ ALTER TABLE `registered_students`
 --
 
 --
+-- AUTO_INCREMENT for table `exam_enquiries`
+--
+ALTER TABLE `exam_enquiries`
+  MODIFY `enqID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `registered_students`
 --
 ALTER TABLE `registered_students`
-  MODIFY `studentID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `studentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12345679;
 
 --
 -- Constraints for dumped tables
